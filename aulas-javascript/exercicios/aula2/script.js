30084da0ccbc97b663f6963516205670209f4ca9
Luiza Jung <luizacojung@gmail.com>
2020-10-21 20:28:03 -0300

    Exercícios de Javascript (1 e 2)

---
+++ aulas-javascript/exercicios/aula2/script.js
@@ -0,0 +1,74 @@
+function saoDiferentes(){
+    document.querySelector('#num1').valueAsNumber;
+    document.querySelector('#num2').valueAsNumber;
+    document.createElement('textoResultado');
+    //document.body.append('Os valores são diferentes!');
+    //textoResultado.style.color = 'red'
+
+    if (num1 == num2) {
+       //alert('meu deus de certo')
+       //div.innerText = 'Os valores são iguais!';
+       document.body.append('Os valores são diferentes!');
+       textoResultado.style.color = 'red'
+    }
+
+    else {
+        //alert('imagina q dlç')
+    }
+}
+
+
+
+
+
+//diferentes.onclick = 
+
+/*diferentes.onclick = function saoDiferentes() {
+    let num1 = document.getElementById('num1').valueAsNumber;
+    let num2 = document.getElementById('num2').valueAsNumber;
+    //div.id = 'textoResultado'
+
+
+    if (num1 != num2) {
+        document.createElement('textoResultado');
+        document.querySelector('textoResultado')
+        textoResultado.innerText = 'Os valores são diferentes!';
+        document.body.append(textoResultado);
+        textoResultado.style.color = 'red'
+    }
+    
+    /*else {
+        let resultado = document.createElement('div');
+        div.innerText = 'Não são diferentes!';
+        document.body.append(resultado);
+    }
+}*/
+
+
+//resete.onclick = 
+
+/*function resetar(resultado) {
+    resultado.remove();
+}*/
+
+/*function saoDiferentes() {
+    let num1 = document.querySelectorId('num1').valueAsNumber;
+    let num2 = document.querySelectorId('num2').valueAsNumber;
+    document.body.append('resposta');
+
+    if (num1 != num2) {
+        resposta.innerText = 'Os valores são diferentes!';
+        resposta.classList.add('diferente');
+
+    }
+    else {
+        resposta.innerText = 'Não são diferentes!';
+        resposta.classList.add('igual');
+    }
+}
+function resetar(resposta) {
+    resposta.remove();
+    document.querySelectorId('num1').valueAsNumber;
+    document.querySelectorId('num2').valueAsNumber;
+}
+*/
